4a47dad3465af46eb65e8ecee42261bc2af3975c
Sagar Yadav <sagarpc1006@gmail.com>
2026-08-05 22:14:40 +0530

    Time: 608 ms (40.18%) | Memory: 0B (100.00%) - LeetSync

--- 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
 
 select employee_id , department_id from Employee 
-where primary_flag = 'Y' or  employee_id not in (select employee_id from Employee where primary_flag = 'Y');
+where primary_flag = 'Y' or employee_id in (select employee_id from Employee group by employee_id having count(*) = 1) 
