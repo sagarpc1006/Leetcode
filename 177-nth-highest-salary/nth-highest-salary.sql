f809d1798236bdfe5d4b74932d580277ee722762
Sagar Yadav <sagarpc1006@gmail.com>
2026-08-19 22:08:20 +0530

    Time: 826 ms (6.86%) | Memory: 0B (100.00%) - LeetSync

---
+++ 177-nth-highest-salary/nth-highest-salary.sql
@@ -0,0 +1,12 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  set N = N - 1 ;
+  RETURN (
+        SELECT DISTINCT salary
+        FROM Employee
+        ORDER BY salary DESC
+        LIMIT 1 OFFSET N
+      
+
+  );
+END
