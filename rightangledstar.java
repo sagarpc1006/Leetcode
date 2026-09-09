5c1d4a491c2e129bb14ae3cc920ef4f0db45d7aa
sagarpc1006 <sagarpc1006@gmail.com>
2026-09-09 20:17:41 +0530

    Patterns-done

---
+++ rightangledstar.java
@@ -0,0 +1,14 @@
+public class rightangledstar {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int space = 5; space > i; space--) {
+                System.out.print("  ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print(" *");
+            }
+            System.out.println("");
+        }
+    }
+
+}
