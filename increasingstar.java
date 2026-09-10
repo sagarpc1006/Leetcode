c31d749724b3b096ceff03078620fa213faaad8f
sagarpc1006 <sagarpc1006@gmail.com>
2026-09-10 07:05:39 +0530

    Increasing-Star

--- increasingstar.java
+++ increasingstar.java
@@ -1,10 +1,17 @@
 public class increasingstar {
     public static void main(String[] args) {
+
+        // Outer for loop
         for (int i = 1; i <= 5; i++) {
+            // Inner space for loop
+            for (int space = 5; space > i; space--) {
+                System.out.print(" ");
+            }
+            // Inner star for loop
             for (int j = 1; j <= i; j++) {
                 System.out.print("* ");
             }
-            System.out.println(" ");
+            System.out.println();
         }
     }
 }
