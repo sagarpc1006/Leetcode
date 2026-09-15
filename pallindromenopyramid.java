04249f6bb0f9cfa2154f7d7eeaf379058f695151
sagarpc1006 <sagarpc1006@gmail.com>
2026-09-15 10:00:29 +0530

    Pallindrome_no

---
+++ pallindromenopyramid.java
@@ -0,0 +1,19 @@
+public class pallindromenopyramid {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5; i++) {
+
+            for (int space = 4; space >= i; space--) {
+                System.out.print("  ");
+            }
+
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j + " ");
+            }
+            for (int j = 2; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
